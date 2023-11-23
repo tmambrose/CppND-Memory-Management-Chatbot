c0d1db4af63c016dcc8661cb81e33dcb5d407770
tmambrose <94181494+tmambrose@users.noreply.github.com>
2023-11-23 14:07:32 -0700

    Update graphnode.h
    Change chatbot to unique_ptr

--- src/graphnode.h
+++ src/graphnode.h
@@ -21,7 +21,7 @@ private:
 
     // data handles (not owned)
     std::vector<GraphEdge *> _parentEdges; // edges to preceding nodes 
-    ChatBot *_chatBot = new ChatBot();
+    std::unique_ptr<ChatBot> _chatBot = std::make_unique<ChatBot>();
 
     ////
     //// EOF STUDENT CODE
