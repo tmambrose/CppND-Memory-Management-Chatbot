beebf2d41440699dee74fabf19b0f1cb242b81be
tmambrose <94181494+tmambrose@users.noreply.github.com>
2023-11-23 13:56:15 -0700

    Update graphnode.cpp
    Modify assignment

--- src/graphnode.cpp
+++ src/graphnode.cpp
@@ -37,7 +37,7 @@ void GraphNode::AddEdgeToChildNode(std::unique_ptr<GraphEdge> edge)
 ////
 void GraphNode::MoveChatbotHere(ChatBot&& chatbot)
 {
-    _chatBot = std::move(chatbot);
+    _chatBot = std::move(*chatbot);
     _chatBot->SetCurrentNode(this);
 }
 
