fbb4c44c2e3205df85d2f75bca4ca79fd4779b3b
tmambrose <94181494+tmambrose@users.noreply.github.com>
2023-11-23 13:45:18 -0700

    Update graphnode.cpp
    Change MoveChatbotHere is take ownership of chatbot

--- src/graphnode.cpp
+++ src/graphnode.cpp
@@ -35,9 +35,9 @@ void GraphNode::AddEdgeToChildNode(std::unique_ptr<GraphEdge> edge)
 
 //// STUDENT CODE
 ////
-void GraphNode::MoveChatbotHere(ChatBot *chatbot)
+void GraphNode::MoveChatbotHere(ChatBot&& chatbot)
 {
-    _chatBot = chatbot;
+    _chatBot = std::move(chatbot);
     _chatBot->SetCurrentNode(this);
 }
 
